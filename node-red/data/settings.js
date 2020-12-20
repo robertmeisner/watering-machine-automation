c61eefdd4201b44c069079e856bbaa748eb29365
Robert Meisner <robert@catchit.pl>
2020-12-20 15:40:40 +0100

    allow ipv6 connections

--- node-red/data/settings.js
+++ node-red/data/settings.js
@@ -22,7 +22,7 @@ module.exports = {
     // To listen on all IPv6 addresses, set uiHost to "::",
     // The following property can be used to listen on a specific interface. For
     // example, the following would only allow connections from the local machine.
-    //uiHost: "127.0.0.1",
+    uiHost: "::",
 
     // Retry time in milliseconds for MQTT connections
     mqttReconnectTime: 15000,
