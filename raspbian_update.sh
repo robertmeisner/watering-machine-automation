86f57329b806007ed3029e6d67871cf5b5216665
Robert Meisner <robert@catchit.pl>
2021-08-03 14:22:28 +0200

    replace flows.json

--- raspbian_update.sh
+++ raspbian_update.sh
@@ -2,5 +2,5 @@
 cd $HOME/.node-red
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 node-red-stop
-curl https://raw.githubusercontent.com/robertmeisner/watering-machine-automation/main/node-red/data/flows.json > flows_raspberrypi.json
+curl https://raw.githubusercontent.com/robertmeisner/watering-machine-automation/main/node-red/data/flows.json > flows.json
 node-red-restart
